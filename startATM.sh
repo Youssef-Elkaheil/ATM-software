1ae2ecf0372b4cd05ea196e6ec0996d3c11e407c
youssef <youssefhussein9880@gmail.com>
2024-03-06 00:05:36 +0200

    modified:   startATM.sh

--- startATM.sh
+++ startATM.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-source ~/Desktop/setEmail.sh
+source ~/setEmail.sh
 
 cd ~/Desktop/ATM-software/
 
 # uncomment when using raspberry pi
-#export RASPBERRY_PI=1
+export RASPBERRY_PI=1
 
 python3 ./main.py
